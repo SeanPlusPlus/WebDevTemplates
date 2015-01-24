c762f59aed98d5590c7ad4cdaa799bfea34c45ec
Sean Stephenson <sms@pixar.com>
2015-01-24 14:33:14 -0800

    update ng cloak

--- hello-world/hello-js/app.js
+++ hello-world/hello-js/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
-var hello = angular.module('hello', []);
+var hello = angular.module('hello', ['hello.controllers']);
+angular.module('hello.controllers', []).
+  controller('MainCtrl', ['$scope', function ($scope) {
 
-
+    // hello msg
+    $scope.hello_msg = 'Hello World';
+  }]);
