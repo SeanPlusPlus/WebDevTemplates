0d1aa255a4b784ca98dd7962596b816e0dea721e
Sean Stephenson <sms@pixar.com>
2015-01-24 14:35:04 -0800

    refactor hello js

--- hello-world/hello-js/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var hello = angular.module('hello', ['hello.controllers']);
-angular.module('hello.controllers', []).
-  controller('MainCtrl', ['$scope', function ($scope) {
-
-    // hello msg
-    $scope.hello_msg = 'Hello World';
-  }]);
