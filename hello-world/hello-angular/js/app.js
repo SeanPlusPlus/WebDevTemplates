32bb9951767718bd841b9966a8b3836cd43e61fb
Sean Stephenson <sms@pixar.com>
2015-02-05 18:40:25 -0800

    include hello angular

---
+++ hello-world/hello-angular/js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var hello = angular.module('hello', ['hello.controllers']);
+angular.module('hello.controllers', []).
+  controller('MainCtrl', ['$scope', function ($scope) {
+
+    // hello msg
+    var hello_msg = 'Hello World';
+    $scope.hello_msg = hello_msg;
+    console.log(hello_msg);
+  }]);
