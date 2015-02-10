4e681d28bc0dad9f72742c17dc5e8914748467e2
Sean Stephenson <sms@pixar.com>
2015-02-09 18:14:15 -0800

    update cache script with comments

--- ajax/github/api/cacheUserData.sh
+++ ajax/github/api/cacheUserData.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-users=("seanplusplus" "dlbewley")
+# array of github users
+users=(
+  "seanplusplus"
+  "dlbewley"
+  "cwalkatron"
+)
+
+# loop through user array
 for user in "${users[@]}"
 do
+
+  # local file for data caching
   file="./users/$user"
+
+  # github api url
   url="https://api.github.com/users/$user"
+
+  # curl command to fetch and save data
   curl -o $file $url
 done
 
