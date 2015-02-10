a383406cca85908715ba9ff0403b9b5a80ce11a1
Sean Stephenson <sms@pixar.com>
2015-02-09 16:38:32 -0800

    refactor ajax to $q

--- ajax/github/js/app.js
+++ ajax/github/js/app.js
@@ -2,15 +2,19 @@
 
 var hello = angular.module('hello', ['hello.controllers', 'hello.services']);
 angular.module('hello.controllers', []).
-  controller('MainCtrl', ['$scope', 'helloService', function ($scope, helloService) {
+  controller('MainCtrl', ['$scope', 'helloService', '$q', function ($scope, helloService, $q) {
 
     // hello msg
     var hello_msg = 'Hitting the GitHub API';
     $scope.hello_msg = hello_msg;
 
-    // AJAX call
-    var request = {'method': 'get', 'resource': 'users/seanplusplus'};
-    helloService.async(request).then(function(response) {
-      $scope.user = response.data;
+    // build list of AJAX requests
+    var requests = [
+      helloService.async({'method': 'get', 'resource': 'users/seanplusplus'})
+    ];
+
+    // $q lets us make multiple AJAX calls
+    $q.all(requests).then(function(response) {
+      $scope.user = response[0].data;
     });
   }]);
