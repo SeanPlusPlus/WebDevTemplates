7f1aa2b5e995983e5ee99a8e04ef3a6016501cca
Sean Stephenson <sms@pixar.com>
2015-02-12 14:24:27 -0800

    add ctrl

--- data-viz/d3/js/app.js
+++ data-viz/d3/js/app.js
@@ -1,1 +1,5 @@
-var myApp = angular.module('myApp', ['myApp.directives']);
+var myApp = angular.module('myApp', ['myApp.controllers', 'myApp.directives']);
+angular.module('myApp.controllers', []).
+  controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.data = "42,4,55,15,16,33,52,2"
+  }]);
