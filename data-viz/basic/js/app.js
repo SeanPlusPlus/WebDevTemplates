35d2f99ed9797756ae64c458f160e93daf5bf190
Sean Stephenson <sms@pixar.com>
2015-02-13 15:58:48 -0800

    big refactor for ng 1.3

--- data-viz/basic/js/app.js
+++ data-viz/basic/js/app.js
@@ -1,35 +1,38 @@
-angular.module('plunker', ['angularCharts']);
+'use strict';
 
-function MainCtrl($scope) {
-  $scope.config = {
-    title: 'Products',
-    tooltips: true,
-    labels: false,
-    mouseover: function() {},
-    mouseout: function() {},
-    click: function() {},
-    legend: {
-      display: true,
-      //could be 'left, right'
-      position: 'right'
-    }
-  };
+var hello = angular.module('hello', ['hello.controllers', 'angularCharts']);
 
-  $scope.data = {
-    series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
-    data: [{
-      x: "Laptops",
-      y: [100, 500, 0],
-      tooltip: "this is tooltip"
-    }, {
-      x: "Desktops",
-      y: [300, 100, 100]
-    }, {
-      x: "Mobiles",
-      y: [351]
-    }, {
-      x: "Tablets",
-      y: [54, 0, 879]
-    }]
-  };
-}
+angular.module('hello.controllers', []).
+  controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.config = {
+      title: 'Products',
+      tooltips: true,
+      labels: false,
+      mouseover: function() {},
+      mouseout: function() {},
+      click: function() {},
+      legend: {
+        display: true,
+        //could be 'left, right'
+        position: 'right'
+      }
+    };
+
+    $scope.data = {
+      series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
+      data: [{
+        x: "Laptops",
+        y: [100, 500, 0],
+        tooltip: "this is tooltip"
+      }, {
+        x: "Desktops",
+        y: [300, 100, 100]
+      }, {
+        x: "Mobiles",
+        y: [351]
+      }, {
+        x: "Tablets",
+        y: [54, 0, 879]
+      }]
+    };
+  }]);
