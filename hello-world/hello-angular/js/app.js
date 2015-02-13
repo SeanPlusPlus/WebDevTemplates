03ada4f59b84bedb3601e8b33eed66b1653ab457
Sean Stephenson <sms@pixar.com>
2015-02-13 13:31:04 -0800

    temp commit

--- hello-world/hello-angular/js/app.js
+++ hello-world/hello-angular/js/app.js
@@ -1,23 +1,11 @@
-'use strict';
-
 var hello = angular.module('hello', ['hello.controllers']);
+
 angular.module('hello.controllers', []).
   controller('MainCtrl', ['$scope', function ($scope) {
 
+    'use strict';
+
     // hello msg
-    var hello_msg = 'Some Awesome Things';
-    $scope.hello_msg = hello_msg;
-
-    // awesome_things
-    var awesome_things = [
-      {'name': 'California'},
-      {'name': 'Backpacking'},
-      {'name': 'Chess'}
-    ];
-
-    // loop over awesome things and add exclamation pt!
-    angular.forEach(awesome_things, function(awesome_thing) {
-      awesome_thing.name += '!';
-    });
-    $scope.awesome_things = awesome_things;
+       var helloMsg = 'Some Awesome Things';
+    $scope.helloMsg = helloMsg;
   }]);
