5d14ec491ca0ec52e808af8c0b1fe4ec207bae27
Sean Stephenson <sms@pixar.com>
2015-02-13 16:12:14 -0800

    stub out dummy data for viz

--- data-viz/basic/js/app.js
+++ data-viz/basic/js/app.js
@@ -5,7 +5,7 @@ var hello = angular.module('hello', ['hello.controllers', 'angularCharts']);
 angular.module('hello.controllers', []).
   controller('MainCtrl', ['$scope', function ($scope) {
     $scope.config = {
-      title: 'Products',
+      title: 'Awesome things',
       tooltips: true,
       labels: false,
       mouseover: function() {},
@@ -19,20 +19,23 @@ angular.module('hello.controllers', []).
     };
 
     $scope.data = {
-      series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
+      series: ['60 and up', '30 - 59', '0 - 29'],
       data: [{
-        x: "Laptops",
-        y: [100, 500, 0],
+        x: "California",
+        y: [100, 500, 80],
         tooltip: "this is tooltip"
       }, {
-        x: "Desktops",
-        y: [300, 100, 100]
+        x: "Backpacking",
+        y: [300, 100, 490]
       }, {
-        x: "Mobiles",
-        y: [351]
+        x: "Chess",
+        y: [35, 201, 12]
       }, {
-        x: "Tablets",
-        y: [54, 0, 879]
+        x: "Beer",
+        y: [651, 55, 200]
+      }, {
+        x: "Bikes",
+        y: [54, 30, 879]
       }]
     };
   }]);
