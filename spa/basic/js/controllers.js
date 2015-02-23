5da59cc413fdc7b429ffe21c3595d26e46095e14
Sean Stephenson <sms@pixar.com>
2015-02-23 14:19:05 -0800

    add routes to spa

--- spa/basic/js/controllers.js
+++ spa/basic/js/controllers.js
@@ -1,15 +1,26 @@
 'use strict';
 
 angular.module('hello.controllers', []).
-  controller('MainCtrl', ['$scope', 'helloService', function ($scope, helloService) {
-
-    // hello msg
-    var hello_msg = 'Basic Single Page App';
-    $scope.hello_msg = hello_msg;
+  controller('HomeCtrl', ['$scope', 'helloService', function ($scope, helloService) {
 
     // AJAX call
     var request = {'method': 'get', 'resource': 'awesome.json'};
     helloService.async(request).then(function(response) {
       $scope.awesome_things = response.data;
+    });
+  }]).
+
+  controller('ThingCtrl', ['$scope', 'helloService', '$routeParams', function ($scope, helloService, $routeParams) {
+
+    console.log($routeParams);
+
+    // AJAX call
+    var request = {'method': 'get', 'resource': 'awesome.json'};
+    helloService.async(request).then(function(response) {
+      angular.forEach(response.data, function(thing) {
+        if (thing.name === $routeParams.thing) {
+          $scope.thing = thing;
+        }
+      });
     });
   }]);
