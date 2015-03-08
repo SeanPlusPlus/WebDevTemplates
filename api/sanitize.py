91fc5eda0c5ff52c5464a1640d881431f4ca4beb
Sean Stephenson <sms@pixar.com>
2015-03-08 12:02:29 -0700

    extend tag params for sanitizing

--- api/sanitize.py
+++ api/sanitize.py
@@ -5,5 +5,11 @@ def tag(req, tags):
         return data
     if 'name' not in req:
         data['error'] = {'message': 'name required', 'code': 400}
-        return data
+        return data 
+    if isiinstance(req['name'], basestring) == False:
+        data['error'] = {'message': 'name must be string', 'code': 400}
+        return data 
+    if len(req['name']) == 0:
+        data['error'] = {'message': 'name must be greater than zero length', 'code': 400}
+        return data 
     return req
