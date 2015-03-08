49c2f3e4ed53ae07b64f48bbcc9d8def2ec74be9
Sean Stephenson <sms@pixar.com>
2015-03-08 12:46:04 -0700

    generate random word for new tag

--- api/client.py
+++ api/client.py
@@ -2,6 +2,8 @@
 import pycurl
 from StringIO import StringIO
 import json
+import random
+
 
 # make sure to:
 # 
@@ -11,18 +13,70 @@ import json
 # this script is an example of a standalone client that another user
 # would use to access your api (just like the curl commands
 # in the readme file, but in a full-blow python script instead).
+# running this script will post an example tag, and will also get
+# a list of all current tags. make sure you setup the app first tho.
 
-def main():
-    buffer = StringIO()
+
+###############################################################################
+def generateRandomTag():
+###############################################################################
+
+    word_file = "/usr/share/dict/words"
+    WORDS = open(word_file).read().splitlines()
+    return WORDS[ random.randint(0, len(WORDS)) ]
+
+
+###############################################################################
+def post():
+###############################################################################
+
+    # initialize 
+    api = 'http://localhost:5000/tags'
+    data = json.dumps({"name": generateRandomTag()})
+
+    # run 
     c = pycurl.Curl()
-    c.setopt(c.URL, 'http://localhost:5000/tags')
+    c.setopt(pycurl.URL, api)
+    c.setopt(pycurl.HTTPHEADER, ['Content-Type: application/json'])
+    c.setopt(pycurl.POST, 1)
+    c.setopt(pycurl.POSTFIELDS, data)
+    c.perform()
+    c.close()
+
+
+###############################################################################
+def get():
+###############################################################################
+
+    # initialize
+    buffer = StringIO()
+    api = 'http://localhost:5000/tags'
+
+    # run
+    c = pycurl.Curl()
+    c.setopt(c.URL, api)
     c.setopt(c.WRITEDATA, buffer)
     c.perform()
     c.close()
 
+    # display
     body = buffer.getvalue()
     data = json.loads(body)
     print data
 
+
+###############################################################################
+def main():
+###############################################################################
+
+    print '\n*** POST ***\n'
+    post()
+
+    print '\n*** GET ***\n'
+    get()
+
+
+###############################################################################
 if __name__ == '__main__':
     main()
+###############################################################################
