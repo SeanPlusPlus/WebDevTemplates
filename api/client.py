1ac634786d9c72cc010005f69fda289f2dcf2a24
Sean Stephenson <sms@pixar.com>
2015-03-08 12:30:18 -0700

    add python client example

---
+++ api/client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import pycurl
+from StringIO import StringIO
+
+# make sure to:
+# 
+# pip install pycurl
+# 
+# we are not including pycurl in the requirements.txt file because
+# this script is an example of a standalone client that another user
+# would use to access your api (just like the curl commands
+# in the readme file, but in a full-blow python script instead).
+
+def main():
+    buffer = StringIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'http://localhost:5000/tags')
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+
+    body = buffer.getvalue()
+    print body
+    print len(body)
+
+if __name__ == '__main__':
+    main()
