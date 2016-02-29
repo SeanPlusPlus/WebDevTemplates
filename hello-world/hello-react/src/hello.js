bdda0de0b06cee4c39b0c68f537620e7ebe7b5cb
Sean Stephenson <sms@pixar.com>
2016-02-29 15:56:33 -0800

    include bootstrap modal

--- hello-world/hello-react/src/hello.js
+++ hello-world/hello-react/src/hello.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as $ from 'jquery';
-import { Alert } from 'react-bootstrap';
+import { Alert, Modal, Button } from 'react-bootstrap';
 
 var HelloAjax = React.createClass({
   getInitialState: function() {
     return {
       message: '',
+      detail: '',
+      showModal: false,
     };
   },
 
@@ -16,6 +18,7 @@ var HelloAjax = React.createClass({
       var data = result;
       this.setState({
         message: data.message,
+        detail: data.detail,
       });
     }.bind(this));
   },
@@ -24,11 +27,34 @@ var HelloAjax = React.createClass({
     this.serverRequest.abort();
   },
 
+  close() {
+    this.setState({ showModal: false });
+  },
+
+  open() {
+    this.setState({ showModal: true });
+  },
+
   render: function() {
     return (
-      <Alert bsStyle='success'>
-        {this.state.message}
-      </Alert>
+      <div>
+        <Alert bsStyle='success'>
+          <a href='#' onClick={this.open}>
+            <strong>Success!</strong> {this.state.message}
+          </a>
+        </Alert>
+        <Modal show={this.state.showModal} onHide={this.close} bsSize='large'>
+          <Modal.Header closeButton>
+            <Modal.Title>Request Detail</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <code>{this.state.detail}</code>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     );
   },
 });
