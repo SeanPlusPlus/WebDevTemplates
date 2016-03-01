23e690e63b5f8054e82945494507eb1a9c9b6647
Sean Stephenson <sms@pixar.com>
2016-02-29 22:46:53 -0800

    trim down the tests

--- single-page-app/react/__tests__/HelloWorld-test.js
+++ single-page-app/react/__tests__/HelloWorld-test.js
@@ -1,5 +1,5 @@
 // __tests__/HelloWorld-test.js
-var path = '../src/home';
+var path = '../src/components/home';
 
 jest.dontMock(path)
     .dontMock('jquery');
@@ -8,20 +8,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
-const HelloWorld = require(path).default;
+const Home = require(path).default;
 
 describe('HelloWorld', () => {
-  it('displays hello world', function() {
+  it('loads jquery', function() {
     var $ = require('jquery');
-
-    // Render a hell world in the document
-    var hello = TestUtils.renderIntoDocument(
-      <HelloWorld />
-    );
-
-    var helloNode = ReactDOM.findDOMNode(hello);
-
-    // Verify that it's all good
-    expect(helloNode.textContent).toEqual('Hello World');
   });
 });
